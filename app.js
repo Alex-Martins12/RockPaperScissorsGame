5734873531e3a39514d80ef2c689caf508f512ba
Alex Oscar Martins <89490539+Alex-Martins12@users.noreply.github.com>
2021-08-25 22:11:28 -0300

    1.1
    new feature almost ready, I just need to change the static text inside the text when the language gets changed (line 329...)

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // variables:
 let userScore = 0;
 let computerScore = 0;
+let language = "eng";
 
 //DOM variables
 //caching the DOM
@@ -8,14 +9,21 @@ let computerScore = 0;
 //to do that, we use the id of the html element!
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
-const scoreBoard_div = document.querySelector(".socre-board");
 // ".result > p" because the text we want to change is inside a p tag!!
 const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
+const pt_div = document.getElementById("pt");
+const eng_div = document.getElementById("eng");
+const header_h1 = document.querySelector("header > h1");
+const badgeUser_div = document.getElementById("user-label");
+const badgeComp_div = document.getElementById("computer-label");
+const actionMsg_p = document.getElementById("action-message");
+const hint_h1 = document.querySelector(".wrapper > h1");
+const pt_btn = document.getElementById("pt");
+const eng_btn = document.getElementById("eng");
 
-//some change!!!!
 
 
 // --------------------Functions------------------------
@@ -48,86 +56,164 @@ function getComputerChoice() {
 
 
 
-function convertToWord(letter) {
+function convertToWord(letter, lang) {
     // a easier way to write if/else statements:
-    if (letter === "r") return "Rock";
-    if (letter === "p") return "Paper";
-    return "Scissors";
+    if (lang === "eng") {
+        if (letter === "r") return "Rock";
+        if (letter === "p") return "Paper";
+        return "Scissors";
+    } else {
+        if (letter === "r") return "Pedra";
+        if (letter === "p") return "Papel";
+        return "Tesoura";
+    }
 }
 
 
-function getMessage(user, comp) {
+function getMessage(user, comp, lang) {
 
     //to change the font-size of something in javascript, just write: .fontsize($number);
     //the sup(); / sub(); methods:
     //they supscript or subscript some text (supscript = moves half a line up, comparing to the text on its line), and (subscript = moves half a line down, comparing to the text on its line)
     const smallUserWord = "user".fontsize(3).sup();
     const smallCompWord = "comp".fontsize(3).sup();
+    const smallUserPt = "jogador".fontsize(3).sup();
+    const smallCompPt = "pc".fontsize(3).sup();
     let userFirst = true;
     let message = "";
 
-    //Rock crushes Scissors
-    //Scissors cuts Paper
-    //Paper covers Rock
-    switch (user + comp) {
-
-        //wins first:
-        case "rs":
-            //Rock crushes Scissors
-            message = "crushes";
-            break;
+    //checks the lang option
+    if (lang === "eng") {
         
-        case "sp":
-            //Scissors cuts Paper
-            message = "cuts";
-            break;    
-    
-        case "pr":
-            //Paper covers Rock
-            message = "covers";
-            break;
-    
+        //Rock crushes Scissors
+        //Scissors cuts Paper
+        //Paper covers Rock
+        switch (user + comp) {
 
-        //losts:
-        case "sr":
-            //Rock crushes Scissors
-            message = "crushes";
-            userFirst = false;
-            break;
-        
-        case "ps":
-            //Scissors cuts Paper
-            message = "cuts";
-            userFirst = false;
-            break;    
+            //wins first:
+            case "rs":
+                //Rock crushes Scissors
+                message = "crushes";
+                break;
+
+            case "sp":
+                //Scissors cuts Paper
+                message = "cuts";
+                break;
+
+            case "pr":
+                //Paper covers Rock
+                message = "covers";
+                break;
+
+
+            //losts:
+            case "sr":
+                //Rock crushes Scissors
+                message = "crushes";
+                userFirst = false;
+                break;
+
+            case "ps":
+                //Scissors cuts Paper
+                message = "cuts";
+                userFirst = false;
+                break;
+
+            case "rp":
+                //Paper covers Rock
+                message = "covers";
+                userFirst = false;
+                break;
+
+
+            //draws:
+            case "rr":
+                message = "equals";
+                break;
+
+            case "ss":
+                message = "equals";
+                break;
+
+            case "pp":
+                message = "equals";
+                break;
+        }
+
+        //returns the message
+        if (userFirst === true) {  
+
+            return `${convertToWord(user, lang)}${smallUserWord} ${message} ${convertToWord(comp, lang)}${smallCompWord}.`;
+        } else {
     
-        case "rp":
-            //Paper covers Rock
-            message = "covers";
-            userFirst = false;
-            break;
+            return `${convertToWord(comp, lang)}${smallCompWord} ${message} ${convertToWord(user, lang)}${smallUserWord}.`;
+        }
 
-        
-        //draws:
-        case "rr":
-            message = "equals";
-            break;
-        
-        case "ss":
-            message = "equals";
-            break;    
-    
-        case "pp":
-            message = "equals";
-            break;
-    }
-
-    if (userFirst === true) {  
-
-        return `${convertToWord(user)}${smallUserWord} ${message} ${convertToWord(comp)}${smallCompWord}.`;
     } else {
 
-        return `${convertToWord(comp)}${smallCompWord} ${message} ${convertToWord(user)}${smallUserWord}.`;
+        //Pedra quebra Tesoura
+        //Tesoura corta Papel
+        //Papel embrulha Pedra
+        switch (user + comp) {
+
+            //wins first:
+            case "rs":
+                //Pedra quebra Tesoura
+                message = "quebra";
+                break;
+
+            case "sp":
+                //Tesoura corta Papel
+                message = "corta";
+                break;
+
+            case "pr":
+                //Papel embrulha Pedra
+                message = "embrulha";
+                break;
+
+
+            //losts:
+            case "sr":
+                //Pedra quebra Tesoura
+                message = "quebra";
+                userFirst = false;
+                break;
+
+            case "ps":
+                //Tesoura corta Papel
+                message = "corta";
+                userFirst = false;
+                break;
+
+            case "rp":
+                //Papel embrulha Pedra
+                message = "embrulha";
+                userFirst = false;
+                break;
+
+
+            //draws:
+            case "rr":
+                message = "é igual a";
+                break;
+
+            case "ss":
+                message = "é igual a";
+                break;
+
+            case "pp":
+                message = "é igual a";
+                break;
+        }
+
+        //returns the message (pt)
+        if (userFirst === true) {
+            return `${convertToWord(user, lang)}${smallUserPt} ${message} ${convertToWord(comp, lang)}${smallCompPt}.`;
+        } else {
+            return `${convertToWord(comp, lang)}${smallCompPt} ${message} ${convertToWord(user, lang)}${smallUserPt}.`;
+        }
     }
 }
 
@@ -136,7 +222,7 @@ function getMessage(user, comp) {
 // set timeout function example:
 //it'll wait 3s until it executes the function
 // setTimeout(function() { console.log("Hello")}, 3000);
-function win(userChoice, computerChoice) {
+function win(userChoice, computerChoice, lang) {
 
     const userChoice_div = document.getElementById(userChoice);
 
@@ -153,7 +239,11 @@ function win(userChoice, computerChoice) {
     // result_p.innerHTML = convertToWord(userChoice) + " beats " + convertToWord(ComputerChoice) +". You win! ";
     //a better way of writing strings and functions/variables in the same line, using backticks (`):
     //result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} beats  ${convertToWord(computerChoice)}${smallCompWord}. You won!`;
-    result_p.innerHTML = `${getMessage(userChoice, computerChoice)} You win!`;
+    if (lang === "eng") {
+        result_p.innerHTML = `${getMessage(userChoice, computerChoice, lang)} You win!`;
+    } else {
+        result_p.innerHTML = `${getMessage(userChoice, computerChoice, lang)} Você venceu!`;
+    }
 
     //when the user wins, add a green-glow to the border of the option picked!!
     //to do that, were just gonna add a class of .green-glow to whichever div the user clicked on
@@ -168,7 +258,7 @@ function win(userChoice, computerChoice) {
 }
 
 
-function lose(userChoice, computerChoice) {
+function lose(userChoice, computerChoice, lang) {
 
     const userChoice_div = document.getElementById(userChoice);
 
@@ -177,7 +267,11 @@ function lose(userChoice, computerChoice) {
     computerScore_span.innerHTML = computerScore;
 
     // result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} loses to  ${convertToWord(computerChoice)}${smallCompWord}. You lost...`;
-    result_p.innerHTML = `${getMessage(userChoice, computerChoice)} You lost...`;
+    if (lang === "eng") {
+        result_p.innerHTML = `${getMessage(userChoice, computerChoice, lang)} You lost...`;
+    } else {
+        result_p.innerHTML = `${getMessage(userChoice, computerChoice, lang)} Você perdeu...`;
+    }
 
     //adds the glow to the user choice
     userChoice_div.classList.add('red-glow');
@@ -188,12 +282,16 @@ function lose(userChoice, computerChoice) {
 
 
 
-function draw(userChoice, computerChoice) {
+function draw(userChoice, computerChoice, lang) {
 
     const userChoice_div = document.getElementById(userChoice);
     
     // result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} equals  ${convertToWord(ComputerChoice)}${smallCompWord}. It's a draw!`;
-    result_p.innerHTML = `${getMessage(userChoice, computerChoice)} It's a draw!`;
+    if (lang === "eng") {
+        result_p.innerHTML = `${getMessage(userChoice, computerChoice, lang)} It's a draw!`;
+    } else {
+        result_p.innerHTML = `${getMessage(userChoice, computerChoice, lang)} Empatou!`;
+    }
     
     //adds the glow to the user choice
     userChoice_div.classList.add('gray-glow');
@@ -201,8 +299,54 @@ function draw(userChoice, computerChoice) {
 }
 
 
+function changeLang(lang) {
+    
+    //returns true or false:
+    //element.classList.contains(class);
+    switch (lang) {
 
-function game(userChoice) {
+        case "pt":
+
+            language = "pt";
+
+            if (pt_div.classList.contains('active') === false) {
+                pt_div.classList.add('active');
+                eng_div.classList.remove('active');
+            }
+            break;
+
+        case "eng":
+            
+            language = "eng";
+
+            if (eng_div.classList.contains('active') === false) {
+                eng_div.classList.add('active');
+                pt_div.classList.remove('active');
+            }
+            break;
+    }
+
+    //now we just need to change the text on the page according to the language selected
+
+    /*
+    rockpaperscissors (header) => header_h1
+
+    user/comp inside the badges
+    //user => badgeUser_div
+    //comp => badgeComp_div
+
+    main message/who won => result_p
+
+    make your move => actionMsg_p
+
+    select your language => hint_h1
+
+    buttons => pt_btn, eng_btn
+    */
+}
+
+
+function game(userChoice, lang) {
 
     const ComputerChoice = getComputerChoice();
     
@@ -214,14 +358,14 @@ function game(userChoice) {
         case "rs":
         case "pr":
         case "sp":
-            win(userChoice, ComputerChoice);
+            win(userChoice, ComputerChoice, lang);
             break;
         
         //cases when the user loses:
         case "rp":
         case "ps":  
         case "sr":
-            lose(userChoice, ComputerChoice);
+            lose(userChoice, ComputerChoice, lang);
             break;
         
 
@@ -229,26 +373,20 @@ function game(userChoice) {
         case "rr":
         case "pp":
         case "ss":
-            draw(userChoice, ComputerChoice);
+            draw(userChoice, ComputerChoice, lang);
             break;
     }
 }
 
-
 // now, everytime the user clicks on one of the option images, we need to store the value of the option they picked, and then later on compare it to the computer choice
 function main() {
 
-    //old way of writing the one line function:
-    //rock_div.addEventListener('click', function() {
-    //     game("r");
-    // })
+    pt_div.addEventListener('click', () => changeLang("pt"));
+    eng_div.addEventListener('click', () => changeLang("eng"));
 
-    //new/better way:
-    rock_div.addEventListener('click', () => game("r"));
-    
-    paper_div.addEventListener('click', () => game("p"));
-    
-    scissors_div.addEventListener('click', () => game("s"));
+    rock_div.addEventListener('click', () => game("r", language));
+    paper_div.addEventListener('click', () => game("p", language));
+    scissors_div.addEventListener('click', () => game("s", language)); 
 }
 
 //runs the main function (executes the entire game)
